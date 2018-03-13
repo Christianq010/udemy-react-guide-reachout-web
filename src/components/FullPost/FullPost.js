292e2acde37cb0573df34c9f80461effff3e7daf
Christiaan Quyn <christianq010@gmail.com>
2018-03-13 19:24:25 +0530

    Fix: correct if loop to encapsulate jsx

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -6,17 +6,17 @@ class FullPost extends Component {
     render () {
         let post = <p>Please select a Post!</p>;
         // show full post only when ID exists - (via click)
-        if (this.props.id)
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
+        if (this.props.id) {
+            post = (
+                <div className="FullPost">
+                    <h1>Title</h1>
+                    <p>Content</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
                 </div>
-            </div>
-
-        );
+            );
+        }
         return post;
     }
 }
