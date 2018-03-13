1cdf128d6b3f742a45a3528da03e6e127a6f8a61
Christiaan Quyn <christianq010@gmail.com>
2018-03-13 18:51:40 +0530

    Feat: Create an http request to get data and rendering fetched data to the screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,27 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    // return json response, async requests
+    componentDidMount () {
+        // axios uses promises and returns a JS promise
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({posts: response.data})
+                // console.log(response);
+            });
+    }
     render () {
+        // call our posts and map them into a posts variable and then output our <Post /> element
+        const posts = this.state.posts.map (post => {
+            return <Post key={post.id} title={post.title} author={post.userId} />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
