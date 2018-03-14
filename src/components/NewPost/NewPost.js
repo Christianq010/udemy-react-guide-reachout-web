26af01253093f81756010f0b22fbc20ce79305a1
Christiaan Quyn <christianq010@gmail.com>
2018-03-14 22:01:18 +0530

    Feat: POST data to a server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,6 +8,20 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    // Send a post request to the server
+    // axios package automatically stringyfies this data for us
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts/', data)
+            .then(reponse => {
+                console.log(reponse);
+            });
     }
 
     render () {
@@ -22,7 +37,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
