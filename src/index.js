b2083c2ec172eab4461137aecb904e59c62d824d
Christiaan Quyn <christianq010@gmail.com>
2018-03-15 11:14:57 +0530

    Feat: Global Interceptors for GET/POST requests and accepting Data from an API / server etc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// axios global Interceptors for sending and accepting Data from an API / server etc for everything
+// saved as variables
+const request = axios.interceptors.request.use(request => {
+                    console.log(request);
+                    // edit request config and recieve it
+                    return request;
+                }, error => {
+                    console.log(error);
+                    return Promise.reject(error);
+                });
+const response = axios.interceptors.response.use(response => {
+                    console.log(response);
+                    return response;
+                }, error => {
+                    console.log(error);
+                    return Promise.reject(error);
+                });
+// Called eject to stop them from working
+axios.interceptors.request.eject(request, response);
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
